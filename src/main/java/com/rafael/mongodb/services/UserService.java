128349417b80ed01275ba05becfd84eaf5e99443
Rafael <rafafaine@gmail.com>
2021-05-12 16:13:29 -0300

    Obtendo um usuário por id

--- src/main/java/com/rafael/mongodb/services/UserService.java
+++ src/main/java/com/rafael/mongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.rafael.mongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rafael.mongodb.domain.User;
 import com.rafael.mongodb.domain.repository.UserRepository;
+import com.rafael.mongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,10 @@ public class UserService {
 	public List<User> findAll(){
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
+		
  }
---
+++ src/main/java/com/rafael/mongodb/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,13 @@
+package com.rafael.mongodb.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+	
+
+}
