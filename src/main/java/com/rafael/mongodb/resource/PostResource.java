da2b9fe0b2db581d7d9863a9a4cade12ea9f881a
Rafael <rafafaine@gmail.com>
2021-05-13 17:36:27 -0300

    Obtendo um post por id

---
+++ src/main/java/com/rafael/mongodb/resource/PostResource.java
@@ -0,0 +1,43 @@
+package com.rafael.mongodb.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rafael.mongodb.domain.Post;
+import com.rafael.mongodb.services.PostService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+	@Autowired
+	private PostService service;
+	
+	
+	
+	
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findall() {
+		List<Post> list = service.findAll();
+	
+		return ResponseEntity.ok().body(list);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+		Post obj = service.findById(id);
+
+		return ResponseEntity.ok().body(obj);
+	}
+
+	
+	
+
+}
---
+++ src/main/java/com/rafael/mongodb/services/PostService.java
@@ -0,0 +1,32 @@
+package com.rafael.mongodb.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rafael.mongodb.domain.Post;
+import com.rafael.mongodb.domain.repository.PostRepository;
+import com.rafael.mongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public List<Post> findAll(){
+		return repo.findAll();
+	}
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
+
+ }
