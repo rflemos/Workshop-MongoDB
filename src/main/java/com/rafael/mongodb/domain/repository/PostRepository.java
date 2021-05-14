27479bd4e7787022d615a78ee5ae39edca5e5d7c
Rafael <rafafaine@gmail.com>
2021-05-14 16:05:10 -0300

    Consulta simples com @Query

--- src/main/java/com/rafael/mongodb/domain/repository/PostRepository.java
+++ src/main/java/com/rafael/mongodb/domain/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.rafael.mongodb.domain.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.rafael.mongodb.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>  {
+	//?0 porque e o primeiro parametro do methodo.
+	@Query("{ 'title': { $regex:?0 , $options: 'i'}}")
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);// ja é montado um methodo de busca com query methods.
 	
--- src/main/java/com/rafael/mongodb/services/PostService.java
+++ src/main/java/com/rafael/mongodb/services/PostService.java
@@ -30,7 +30,8 @@ public class PostService {
 		}
 	
 	public List<Post> findByTitle(String text){
-		return repo.findByTitleContainingIgnoreCase(text);
+		return repo.searchTitle(text);
+		//return repo.findByTitleContainingIgnoreCase(text);
 		
 	}
 
